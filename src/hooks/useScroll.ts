ae78f956d7fc1fa519fe47f1eba050389a2172a2
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-27 23:16:52 +0900

    feat: 훅 기능 분리 및 불필요한 코드 제거

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -8,25 +8,17 @@ export const useScroll = () => {
   const setIsColorNav = useSetRecoilState(isColorNavState)
 
   const onScroll = () => {
-    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollY = window.scrollY;
 
-    if (scrollY > 486) setIsColorNav(true)
-    else setIsColorNav(false)
+    if (scrollY > 486) {
+      return setIsColorNav(true)
+    }
+
+    setIsColorNav(false)
+
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', onScroll)
-    return () => window.removeEventListener('scroll', onScroll)
+    window.addEventListener('scroll', onScroll);
   }, [])
-
-  const sectionRef = useRef<HTMLElement>(null);
-
-  const handleMove = (sectionId: string) => {
-    const sectionElement = document.getElementById(sectionId);
-    if (sectionElement) {
-      sectionElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-
-  return { sectionRef, handleMove }
 }
