1649ff749ac35ae9738016fea9ac855293c9d60f
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-27 23:19:22 +0900

    feat: Skill component 커스텀 훅 파일 경로 수정

--- src/component/section/Skill.tsx
+++ src/component/section/Skill.tsx
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
-import { useScroll } from '@/hooks/useScroll';
+import { useMoveToSection } from '@/hooks/useMoveToSection';
 
 const Skill = () => {
-  const { sectionRef } = useScroll();
+  const { sectionRef } = useMoveToSection();
 
   return (
     <SkillBox id='Skill' ref={sectionRef}>
