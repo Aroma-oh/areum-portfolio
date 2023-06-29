b281de02543e73efcc02e3c144d21cd3101f6c5a
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-30 02:03:06 +0900

    feat: headerHeight을 id를 참조하여 가져오는 것으로 변경

--- src/hooks/useMoveToSection.ts
+++ src/hooks/useMoveToSection.ts
@@ -6,7 +6,7 @@ export const useMoveToSection = () => {
   const handleMove = (sectionId: string) => {
 
     const sectionElement = document.getElementById(sectionId);
-    const headerHeight = headerRef.current?.offsetHeight || 63.999; // Intro에서는 header의 높이를 가져올 수 없어서 기본값 63.99 설정함
+    const headerHeight = document.getElementById('Portfolio').offsetHeight
     console.log(headerHeight)
 
     if (sectionElement && headerHeight) {
