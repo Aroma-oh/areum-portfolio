fc83d0d15d658742617c49f0b269818c0e1f0d09
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-29 21:39:02 +0900

    setting: 컴파일 방법 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+
+  experimental: {
+    forceSwcTransforms: true, // 컴파일러 속도 향상
+  },
 }
 
 module.exports = nextConfig
