a5967efc83847bda83f514e361616d3165337a74
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-30 01:23:09 +0900

    feat: header 높이를 상수에서 변수로 변경

--- src/hooks/useMoveToSection.ts
+++ src/hooks/useMoveToSection.ts
@@ -1,15 +1,19 @@
+import { useRef } from 'react';
+
 export const useMoveToSection = () => {
+  const headerRef = useRef<HTMLDivElement | null>(null);
 
   const handleMove = (sectionId: string) => {
 
     const sectionElement = document.getElementById(sectionId);
+    const headerHeight = headerRef.current?.offsetHeight;
 
-    if (sectionElement) {
+    if (sectionElement && headerHeight) {
       const sectionTop = sectionElement.getBoundingClientRect().top;
-      window.scrollTo({ top: window.pageYOffset + sectionTop - 63.999, behavior: 'smooth' });
+      window.scrollTo({ top: window.pageYOffset + sectionTop - headerHeight, behavior: 'smooth' }); // 네비 높이 구해서 63 대체하기
     }
 
   };
 
-  return { handleMove }
+  return { handleMove, headerRef }
 }
