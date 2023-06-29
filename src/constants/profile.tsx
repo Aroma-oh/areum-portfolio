55c37b3b8290fba3f1fac5a23608add693b40e54
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-30 03:06:29 +0900

    feat: 프로필 데이터 파일 생성

---
+++ src/constants/profile.tsx
@@ -0,0 +1,26 @@
+import { FaAddressCard, FaPhoneAlt, FaSchool } from 'react-icons/Fa';
+import { MdMarkEmailUnread } from 'react-icons/md';
+
+
+export const PROFILE = [
+  {
+    icon: <FaAddressCard />,
+    type: 'Name',
+    content: '오아름',
+  },
+  {
+    icon: <FaPhoneAlt />,
+    type: 'PHONE',
+    content: '010-7735-8373',
+  },
+  {
+    icon: <MdMarkEmailUnread />,
+    type: 'EMAIL',
+    content: 'on002way@gamil.com',
+  },
+  {
+    icon: <FaSchool />,
+    type: 'BOOTCAMP',
+    content: '코드스테이츠 43기',
+  },
+];
