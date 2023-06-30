db19cc7d233ee2c2c3fc5bc3424ed090f5116af7
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-01 00:30:01 +0900

    feat: skills type 변경

--- src/types/skills.ts
+++ src/types/skills.ts
@@ -1,6 +1,9 @@
-export interface Skills {
+export type Skills = {
+  stack: string;
   name: string;
   value: number;
-  class: string;
+  className: string;
   content: string;
-}
+};
+
+export type SkillSet = [string, Skills[], () => void];
