e42a7a05201fa9975869e4ddc35c2af2ca1df434
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-06-30 23:26:34 +0900

    feat: skills constant data 수정

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,42 +1,49 @@
 
 export const FRONTEND = [
   {
+    category: 'frontend',
     name: 'React',
     value: 60,
     class: 'bg-react',
     content: '재사용이 가능한 컴포넌트 단위로 개발하고, CSR을 경험해 봤어요.'
   },
   {
+    category: 'frontend',
     name: 'Next.js',
     value: 40,
     class: 'bg-next',
     content: '검색최적화를 위해 SSR과 SSG를 경험해 봤어요.'
   },
   {
+    category: 'frontend',
     name: 'TypeScript',
     value: 40,
     class: 'bg-typeScript',
     content: 'JS를 정적 타입으로 개발하여 서비스의 안정성을 향상시켰어요.'
   },
   {
+    category: 'frontend',
     name: 'Redux-toolkit',
     value: 40,
     class: 'bg-redux-toolkit',
     content: 'Redux-toolkit을 사용하여 전역으로 상태관리를 해봤어요.'
   },
   {
+    category: 'frontend',
     name: 'Recoil',
     value: 40,
     class: 'bg-recoil',
     content: 'Recoil을 사용하여 전역으로 상태관리를 해봤어요.'
   },
   {
+    category: 'frontend',
     name: 'Styled-component',
     value: 60,
     class: 'bg-styled-component',
     content: 'Styled-component를 사용하여 프로젝트를 만들어 봤어요.'
   },
   {
+    category: 'frontend',
     name: 'MUI',
     value: 60,
     class: 'bg-mui',
@@ -46,18 +53,21 @@ export const FRONTEND = [
 
 export const BACKEND = [
   {
+    category: 'backend',
     name: 'Node.js',
     value: 10,
     class: 'bg-node',
     content: 'Node.js로 서버를 구축해 봤어요.'
   },
   {
+    category: 'backend',
     name: 'Express',
     value: 10,
     class: 'bg-express',
     content: 'Express로 서버를 구축해 봤어요.'
   },
   {
+    category: 'backend',
     name: 'Firebase',
     value: 10,
     class: 'bg-firebase',
@@ -67,30 +77,35 @@ export const BACKEND = [
 
 export const ETC = [
   {
+    category: 'etc',
     name: 'Figma',
     value: 60,
     class: 'bg-figma',
     content: 'Figma로 프로젝트를 기획하고 프로토타입을 만들어 봤어요.'
   },
   {
+    category: 'etc',
     name: 'Stoybook',
     value: 20,
     class: 'bg-storybook',
     content: 'Stoybook으로 컴포넌트 기반 UI를 개발하고 문서화 해봤어요.'
   },
   {
+    category: 'etc',
     name: 'Postman',
     value: 40,
     class: 'bg-postman',
     content: 'Postman으로 RESTful API 테스트를 해봤어요.'
   },
   {
+    category: 'etc',
     name: 'GraphQL',
     value: 20,
     class: 'bg-graphql',
     content: 'GraphQL로 클라이언트와 서버 간의 데이터 통신을 해봤어요.'
   },
   {
+    category: 'etc',
     name: 'AWS',
     value: 10,
     class: 'bg-aws',
