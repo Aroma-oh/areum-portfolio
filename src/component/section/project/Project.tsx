bdd8c13583579b124f06da9375c3a54d8f36d4d4
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-03 03:24:47 +0900

    feat: SSR

--- src/component/section/project/Project.tsx
+++ src/component/section/project/Project.tsx
@@ -1,5 +1,4 @@
-// react, styled import 
-import { useState, useEffect } from 'react'
+// styled import 
 import styled from '@emotion/styled';
 // recoil import
 import { useRecoilValue } from 'recoil';
@@ -10,35 +9,20 @@ import { ViewMode } from '@/component/section/project/ViewMode';
 import { NavButton } from '@/component/section/project/NavButton';
 import { Carouser } from '@/component/section/project/Carouser';
 import { Content } from '@/component/section/project/Content';
-// data, type import
-import { PROJECTS } from '@/constants/project'
-import { getDbAllData } from '@/util/firebase';
-import { ProjectNav, ProjectList, Project } from '@/types/project'
+// type import 
+import { ProjectNav, ProjectList } from '@/types/project'
 
-const Project = () => {
+interface ProjectProps {
+  ssrData: {
+    nav: ProjectNav[],
+    project: ProjectList[],
+  }[]
+}
+
+const Project = ({ ssrData }: ProjectProps) => {
   const isHorizon = useRecoilValue(isHorizontalState);
   const selectedProject = useRecoilValue(selectProject);
-  const [navData, setNavData] = useState<ProjectNav[]>([]);
-  const [projectData, setProjectData] = useState<ProjectList[]>([]);
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchedData1 = await getDbAllData<Project>('project');
-        setNavData(fetchedData1[0].nav);
-        setProjectData(fetchedData1[0].project)
-        console.log(fetchedData1[0].project)
-      } catch (error) {
-        console.error('Failed to fetch data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  console.log(navData)
-  console.log(projectData)
+  const { nav, project } = ssrData[0];
 
   return (
     <ProjectBox id='Project'>
@@ -46,21 +30,23 @@ const Project = () => {
       <div className='view-mode'>
         <ViewMode />
       </div>
-      {isHorizon === true && <NavButton navData={navData} />}
+      {isHorizon === true && <NavButton navData={nav} />}
       <TextBox>
         {isHorizon === true
           ?
           <>
-            <Carouser data={PROJECTS[selectedProject]} navData={navData[selectedProject]} projectData={projectData[selectedProject]} />
-            <Content data={PROJECTS[selectedProject]} />
+            <Carouser nav={nav[selectedProject]} project={project[selectedProject]} />
+            <Content project={project[selectedProject]} />
           </>
           :
-          PROJECTS.map((project, idx) => (
-            <div key={idx}>
-              {/* <Carouser data={project} /> */}
-              <Content data={project} />
-            </div>
-          ))
+          // 세로모드 데이터 전달 방안 고민 필요
+          // nav.map((el, idx) => (
+          //   <div key={idx}>
+          //     <Carouser nav={el[idx]} />
+          //     <Content ssrData={ssrData} />
+          //   </div>
+          // ))
+          null
         }
       </TextBox>
     </ProjectBox>
@@ -86,7 +72,6 @@ const ProjectBox = styled.section`
     margin-bottom: 1rem;
   }
 `
-
 const TextBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -95,3 +80,5 @@ const TextBox = styled.div`
 `
 
 export default Project;
+
+
