768f9e4c94524f78cd11f7ce1859696ee4196602
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-02 17:30:00 +0900

    feat: title 추가

--- src/component/section/project/Carouser.tsx
+++ src/component/section/project/Carouser.tsx
@@ -6,7 +6,12 @@ import Carousel from 'react-material-ui-carousel';
 
 interface Props {
   data: {
-    nav: object;
+    nav: {
+      type: string,
+      name: string,
+      create: string,
+      period: number,
+    };
     image: string[];
   }
 }
@@ -15,7 +20,11 @@ export const Carouser = ({ data }: Props) => {
   const { nav, image } = data;
 
   return (
-    <div>
+    <Box >
+      <div className='title-box'>
+        <h5> {nav.name} 프로젝트</h5>
+        <p> 개발기간 : {nav.period}</p>
+      </div>
       <CarouselBox
         navButtonsAlwaysVisible={true}
         interval={3000}
@@ -34,17 +43,32 @@ export const Carouser = ({ data }: Props) => {
               src={el}
               alt=''
               width={500}
-              height={300}
+              height={400}
               unoptimized={false}
             />
           </div>
         ))}
       </CarouselBox>
-    </div>
+    </Box>
 
   )
 }
 
+const Box = styled.div`
+  .title-box {
+    text-align: center;
+    margin: 2rem 0 3.5rem 0;
+  }
+  h5 {
+    margin: 2.5rem 0 0.8rem 0;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #575757;
+  }
+  p {
+    font-size: 1.2rem;
+  }
+`
 const CarouselBox = styled(Carousel)`
   .slide {
     display: flex;
