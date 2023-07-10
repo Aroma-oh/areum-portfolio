6c87e949391ac44e1174c713365c18fbb6e5f39c
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-10 22:22:21 +0900

    feat: 불필요한 코드 제거

--- src/util/firebase/index.ts
+++ src/util/firebase/index.ts
@@ -1,11 +1,6 @@
 import {
-  DocumentData,
   collection,
-  deleteDoc,
-  doc,
-  getDoc,
   getDocs,
-  setDoc,
 } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
@@ -22,47 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-/**
- * 컬렉션명과 이름이 일치하는 데이터의 값을 수정할 수 있음
- * 일치하는 컬렉션명이 없다면 셋팅과 동시에 데이터를 추가할 수 있음
- */
 
-export const setDbData = async <T extends DocumentData>(
-  collectionName: string,
-  id: string,
-  data: T
-) => {
-  const docRef = doc(db, collectionName, id);
-
-  await setDoc(docRef, data);
-};
-
-/**
- * 컬렉션명과 ID가 일치하는 데이터를 삭제하는 함수
- */
-export const deleteDbData = async (collectionName: string, id: string) => {
-  const docRef = doc(db, collectionName, id);
-
-  await deleteDoc(docRef);
-};
-
-/**
- * 단일 데이터 조회
- */
-export const getDbDataByDocName = async <T>(
-  collectionName: string,
-  docName: string
-): Promise<T> => {
-  const docRef = doc(db, collectionName, docName);
-
-  const docSnapshot = await getDoc(docRef);
-
-  return docSnapshot.data() as T;
-};
-
-/**
- * 한 컬렉션에 대한 전체 데이터 조회
- */
 export const getDbAllData = async <T>(collectionName: string): Promise<T[]> => {
   const querySnapshot = await getDocs(collection(db, collectionName));
 
