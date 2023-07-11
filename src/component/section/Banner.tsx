88e1b6a88fff7c16c22d9aff453d6fac46bfd90f
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-11 22:13:28 +0900

    feat: 테스트용 배너 컴포넌트 생성

---
+++ src/component/section/Banner.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Banner = () => {
+  return (
+    <BannerBox>
+      테스트
+    </BannerBox>
+  )
+}
+
+const BannerBox = styled.div`
+  width: 100%;
+`
