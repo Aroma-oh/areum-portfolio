f439e77a277e9ff34fe56928185c8be9fc423206
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-20 14:36:09 +0900

    feat: Mountain canvas 사이즈 설정

--- src/component/section/Mountain.tsx
+++ src/component/section/Mountain.tsx
@@ -6,6 +6,20 @@ const Mountain = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas?.getContext('2d');
+
+    if (!canvas || !ctx) return;
+
+    const handleResize = () => {
+      const devicePixel = window.devicePixelRatio;
+      canvas.width = window.innerWidth * devicePixel;
+      canvas.height = window.innerHeight * devicePixel;
+      ctx.scale(devicePixel, devicePixel);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+
   }, []);
 
   return <canvas ref={canvasRef} />
