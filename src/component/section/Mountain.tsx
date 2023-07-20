a8c2d0a2f77ae393f518793fef24aa724a21f8ff
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-20 16:29:23 +0900

    feat: Hill 추가 중간 저장

--- src/component/section/Mountain.tsx
+++ src/component/section/Mountain.tsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from 'react';
+import { Hill } from '@/component/section/Hill';
 
-const Mountain = () => {
+export const Mountain = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -9,16 +10,36 @@ const Mountain = () => {
 
     if (!canvas || !ctx) return;
 
+    const hills = [
+      new Hill('#DFFFD8', 0.2, 12),
+      new Hill('#B5F1CC', 0.5, 8),
+      new Hill('#95DAC1', 1.2, 6),
+    ];
+
+    const devicePixel = window.devicePixelRatio;
+    const stageWidth = window.innerWidth;
+    const stageHeight = window.innerHeight;
+
     const handleResize = () => {
-      const devicePixel = window.devicePixelRatio;
-      canvas.width = window.innerWidth * devicePixel;
-      canvas.height = window.innerHeight * devicePixel;
-      ctx.scale(devicePixel, devicePixel);
+      canvas.width = stageWidth * devicePixel;
+      canvas.height = stageHeight * devicePixel;
+
+      for (let i = 0; i < hills.length; i++) {
+        hills[i].resize(stageWidth, stageHeight);
+      }
     };
 
     window.addEventListener('resize', handleResize);
     handleResize();
 
+    const animate = () => {
+      ctx.clearRect(0, 0, stageWidth, stageHeight);
+      for (let i = 0; i < hills.length; i++) {
+        hills[i].draw(ctx);
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
 
   }, []);
 
