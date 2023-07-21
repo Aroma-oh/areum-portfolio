2336317e55dcc0651c597adc8529a408c9a3a1bf
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-21 14:09:40 +0900

    feat: 프로필 위치 수정

--- src/component/section/Profile.tsx
+++ src/component/section/Profile.tsx
@@ -8,53 +8,62 @@ const Profile = () => {
   const { handleMove } = useMoveToSection();
 
   return (
-
-    <ProfileBox id='about'>
-      <ul>
-        <li>
-          <span className='underline'><span className='strong'>문제</span>에서 기회를 포착</span>하고, <br /><span className='strong'>해결</span>을 통한 <span className='strong'>성취감</span>을 좋아합니다.</li>
-        <li>
-          일상에서 마주한 불편함으로, <br /> 더 나은 <span className='underline'><span className='strong'>사용자 경험</span>에 대해 고민</span>합니다. </li>
-      </ul>
-      <div className='card-container'>
-        {PROFILE.map(({ icon, type, content }, index) => (
-          <CardBox
-            key={type}
-            animate={{
-              opacity: 1,
-              scale: 1.2,
-            }}
-            transition={{
-              repeat: Infinity,
-              type: 'spring',
-              mass: 3,
-              delay: index * 0.2,
-            }}
-          >
-            <motion.div className='content-box'>
-              <div className='icon'>{icon}</div>
-              <div className='text-box'>
-                <p className='type'>{type}</p>
-                <p className='text'>{content}</p>
+    <AboutBox id='about'>
+      <ProfileBox >
+        <ul>
+          <li>
+            <span className='underline'><span className='strong'>문제</span>에서 기회를 포착</span>하고, <br /><span className='strong'>해결</span>을 통한 <span className='strong'>성취감</span>을 좋아합니다.</li>
+          <li>
+            일상에서 마주한 불편함으로, <br /> 더 나은 <span className='underline'><span className='strong'>사용자 경험</span>에 대해 고민</span>합니다. </li>
+        </ul>
+        <div className='card-container'>
+          {PROFILE.map(({ icon, type, content }, index) => (
+            <CardBox
+              key={type}
+              animate={{
+                opacity: 1,
+                scale: 1.2,
+              }}
+              transition={{
+                repeat: Infinity,
+                type: 'spring',
+                mass: 3,
+                delay: index * 0.2,
+              }}
+            >
+              <div className='content-box'>
+                <div className='icon'>{icon}</div>
+                <div className='text-box'>
+                  <p className='type'>{type}</p>
+                  <p className='text'>{content}</p>
+                </div>
               </div>
-            </motion.div>
-          </CardBox>
-        ))}
+            </CardBox>
+          ))}
+        </div>
+      </ProfileBox>
+      <div>
+        <Button onClick={() => handleMove('skill')} />
       </div>
-      <Button onClick={() => handleMove('Profile')} />
-    </ProfileBox>
+    </AboutBox>
+
   )
 }
-
-const ProfileBox = styled.section`
+const AboutBox = styled.section`
   min-height: calc(100vh - 84px);
-  width: fit-content;
-  margin: auto;
+  width: 100%;
+  padding-top: 3rem;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`
+const ProfileBox = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
 
   @font-face {
     font-family: 'HakgyoansimWoojuR';
@@ -65,17 +74,18 @@ const ProfileBox = styled.section`
 
   ul {
     font-family: 'HakgyoansimWoojuR';
+    height: 600px;
 
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    justify-content: flex-start;
   }
   li {
     width: 30vw;
     font-size: 2rem;
     line-height: 3rem;
-    margin: 2rem 8rem 4rem 8rem;
+    margin: 2vw 8vw 4vw 8vw;
   }
   .strong {
     font-size: 2.5rem;
@@ -94,9 +104,24 @@ const ProfileBox = styled.section`
     justify-content: center;
     flex-direction: column;
   }
+
   @media (max-width: 900px) {
       flex-direction: column;
       align-items: center;
+      margin-bottom: 3rem;
+
+      ul {
+        height: fit-content;
+        margin-bottom: 3rem;
+      }
+      li {
+        font-size: 1.5rem;
+        width: 70vw;
+        text-align: center;
+      }
+      .strong {
+        font-size: 2rem;
+      }
     }
 `
 
