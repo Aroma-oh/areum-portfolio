da520028954d85a01bf2c31169391ccd4e30e5ac
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-23 01:31:36 +0900

    feat: color nav 전역 상태 업데이트 제거

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,10 +1,5 @@
 import { atom } from 'recoil';
 
-export const isColorNavState = atom({
-  key: 'isColorNavState',
-  default: false,
-});
-
 export const selectMenuState = atom({
   key: 'selectMenuState',
   default: '',
@@ -25,7 +20,7 @@ export const isHorizontalState = atom({
   default: true,
 });
 
-export const selectProject = atom({
-  key: 'selectProject',
-  default: 0,
-});
+export const viewSectionState = atom({
+  key: 'viewSectionState',
+  default: 'intro',
+})
