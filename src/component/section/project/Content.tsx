b130ac6d98d7acc1d3e9536b275ab405787b19b6
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-24 11:36:07 +0900

    feat: Content 컴포넌트 분리

---
+++ src/component/section/project/Content.tsx
@@ -0,0 +1,74 @@
+// styled import 
+import styled from '@emotion/styled';
+// type import
+import { ProjectList } from '@/types/project';
+
+interface Props {
+  project: ProjectList;
+};
+
+export const Content = ({ project }: Props) => {
+  return (
+    <ContentBox>
+      <div className='intro'>
+        {project.intro.map(({ title, content }, idx) => (
+          <div key={idx}>
+            <h5>{title}</h5>
+            {content.map((paragraph, idx) => (
+              <div key={`${idx} content`} className='content'>
+                <p>{paragraph}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className='info'>
+        {project.info.map(({ title, content }, idx) => (
+          <div key={idx} className='info-box'>
+            <h6>{title}</h6>
+            <div className='info-text'>{content}</div>
+          </div>
+        ))}
+      </div>
+    </ContentBox>
+  )
+};
+
+const ContentBox = styled.div`
+  width: 35vw;
+  margin-left: 5rem;  
+
+  h5 {
+    margin: 2rem 0 0.8rem 0;
+    font-size: 1.3rem;
+    font-weight: 600;
+    color: #000000c8;
+    width: fit-content;
+
+    background-image: linear-gradient(90deg, #95DAC1, #fffd7f);
+    background-position: bottom;
+    background-size: 100% 50%;
+    background-repeat: no-repeat;
+  }
+  p, .info-text {
+    color: #565656;
+    line-height: 1.3rem;
+    word-break: break-all;
+  }
+  .info {
+    margin-top: 1.8rem;
+  }
+  h6 {
+    font-size: 1rem;
+    font-weight: 600;
+    color: #000000c8;
+    margin: 0.8rem 0;
+    width: fit-content;
+
+    background-image: linear-gradient(90deg, #95DAC1, #fffd7f5a);
+    background-position: bottom;
+    background-size: 100% 50%;
+    background-repeat: no-repeat;
+  }
+`
+
--- src/component/section/project/Detail.tsx
+++ src/component/section/project/Detail.tsx
@@ -7,9 +7,10 @@ import MuiCarousel from 'react-material-ui-carousel';
 // custom hook import
 import { useMoveToSection } from '@/hooks/useMoveToSection';
 // type import
-import { ProjectList } from '@/types/project'
+import { ProjectList } from '@/types/project';
 // 
 import { Carousel } from '@/component/section/project/Carousel'
+import { Content } from '@/component/section/project/Content'
 
 interface Props {
   project: ProjectList;
@@ -25,28 +26,8 @@ export const Detail = memo(({ project }: Props) => {
         handleMove('project');
       }}>
       <Carousel project={project} />
-      <ContentBox>
-        <div className='intro'>
-          {project?.intro.map(({ title, content }, idx) => (
-            <div key={idx}>
-              <h5>{title}</h5>
-              {content.map((paragraph, idx) => (
-                <div key={`${idx} content`} className='content'>
-                  <p>{paragraph}</p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        <div className='info'>
-          {project?.info.map(({ title, content }, idx) => (
-            <div key={idx} className='info-box'>
-              <h6>{title}</h6>
-              <div className='info-text'>{content}</div>
-            </div>
-          ))}
-        </div>
-      </ContentBox>
+      <Content project={project} />
+
     </DetailBox>
   );
 });
@@ -60,42 +41,4 @@ const DetailBox = styled.section`
   justify-content: center;
   align-items: center;
 
-`
-
-const ContentBox = styled.div`
-  width: 35vw;
-  margin-left: 5rem;  
-
-  h5 {
-    margin: 2rem 0 0.8rem 0;
-    font-size: 1.3rem;
-    font-weight: 600;
-    color: #000000c8;
-    width: fit-content;
-
-    background-image: linear-gradient(90deg, #95DAC1, #fffd7f);
-    background-position: bottom;
-    background-size: 100% 50%;
-    background-repeat: no-repeat;
-  }
-  p, .info-text {
-    color: #565656;
-    line-height: 1.3rem;
-    word-break: break-all;
-  }
-  .info {
-    margin-top: 1.8rem;
-  }
-  h6 {
-    font-size: 1rem;
-    font-weight: 600;
-    color: #000000c8;
-    margin: 0.8rem 0;
-    width: fit-content;
-
-    background-image: linear-gradient(90deg, #95DAC1, #fffd7f5a);
-    background-position: bottom;
-    background-size: 100% 50%;
-    background-repeat: no-repeat;
-  }
 `
