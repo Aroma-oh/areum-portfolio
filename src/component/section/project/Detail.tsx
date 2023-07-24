4e9546f055dde66929181c6619cae07dcea49e72
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-24 13:05:11 +0900

    feat: visibility 조건 추가

--- src/component/section/project/Detail.tsx
+++ src/component/section/project/Detail.tsx
@@ -1,4 +1,4 @@
-// styled, react, next.js import 
+// styled, react import 
 import styled from '@emotion/styled';
 import { memo } from 'react';
 // custom hook import
@@ -6,34 +6,51 @@ import { useMoveToSection } from '@/hooks/useMoveToSection';
 // type import
 import { ProjectList } from '@/types/project';
 // component import 
-import { Carousel } from '@/component/section/project/Carousel'
-import { Content } from '@/component/section/project/Content'
+import { Carousel } from '@/component/section/project/Carousel';
+import { Content } from '@/component/section/project/Content';
+import { SubButton } from '@/component/common/SubButton';
+
 
 interface Props {
   project: ProjectList;
+  className: string;
+  handleDetailClose: () => void;
 };
 
-export const Detail = memo(({ project }: Props) => {
+export const Detail = memo(({ project, className, handleDetailClose }: Props) => {
   const { handleMove } = useMoveToSection();
 
   return (
-    <DetailBox
-      id='detail'
-      onClick={() => {
+    <DetailBox id='detail' className={className} >
+      <div className='content'>
+        <Carousel project={project} />
+        <Content project={project} />
+      </div>
+      <SubButton onClick={() => {
         handleMove('project');
-      }}>
-      <Carousel project={project} />
-      <Content project={project} />
+        handleDetailClose();
+      }} />
     </DetailBox>
   );
 });
 
 const DetailBox = styled.section`
+  padding-top: 84px;
   width: 99vw;
-  height: 100vh;
+  height: ${({ className }) => (className === 'open' ? 'calc(100vh - 84px)' : '0px')};
+  visibility: ${({ className }) => (className === 'open' ? 'visible' : 'hidden')};
+  transition: height 2s ease-in;
+
   font-family: 'SUIT-Regular';
-  
+
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
+
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `
