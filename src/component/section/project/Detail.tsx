d0563316c39f7d2c4f30991833e1113e0389c37b
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-24 11:33:09 +0900

    feat: Carousel 컴포넌트 분리

--- src/component/section/project/Detail.tsx
+++ src/component/section/project/Detail.tsx
@@ -8,7 +8,8 @@ import MuiCarousel from 'react-material-ui-carousel';
 import { useMoveToSection } from '@/hooks/useMoveToSection';
 // type import
 import { ProjectList } from '@/types/project'
-
+// 
+import { Carousel } from '@/component/section/project/Carousel'
 
 interface Props {
   project: ProjectList;
@@ -23,42 +24,7 @@ export const Detail = memo(({ project }: Props) => {
       onClick={() => {
         handleMove('project');
       }}>
-      <CarouselBox id='view-project'>
-        <p className='sub-text'>이미지에 마우스를 올리면 auto play를 멈출 수 있습니다.</p>
-        <Carousel
-          interval={3000}
-          animation={"fade"}
-          autoPlay={true}
-          sx={{
-            width: '25vw',
-            height: 'fit-content',
-          }}
-        >
-          {project?.image.map((el, idx) => (
-            <div
-              className='slide'
-              key={idx}
-            >
-              <Image
-                src={el}
-                alt=''
-                width={700}
-                height={400}
-              />
-            </div>
-          ))}
-        </Carousel>
-        <div className='title-box'>
-          <h5> {project?.nav.name} </h5>
-          <p>{project?.nav.type} project</p>
-          {project?.nav.link.map(({ title, href }, index) => (
-            <div key={index} >
-              <p className='title'> {title} </p>
-              <a className='link' href={href} target="_blank" aria-label='프로젝트 링크'>{href}</a>
-            </div>
-          ))}
-        </div>
-      </CarouselBox>
+      <Carousel project={project} />
       <ContentBox>
         <div className='intro'>
           {project?.intro.map(({ title, content }, idx) => (
@@ -73,7 +39,7 @@ export const Detail = memo(({ project }: Props) => {
           ))}
         </div>
         <div className='info'>
-          {project?.info.map(({ title, href, content }, idx) => (
+          {project?.info.map(({ title, content }, idx) => (
             <div key={idx} className='info-box'>
               <h6>{title}</h6>
               <div className='info-text'>{content}</div>
@@ -93,60 +59,7 @@ const DetailBox = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-`
 
-const CarouselBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: fit-content;
-  width: 32vw;
-
-  .title-box {
-    line-height: 1.4rem;
-    h5 {
-      display: inline;
-      font-size: 1.5rem;
-      font-weight: 600;
-      color: #000000c8;
-    }
-    p:nth-child(2) {
-      display: inline;
-      color: #565656;
-    }
-    div:nth-child(3) {
-      padding-top: 1.4rem;
-    }
-    .title {
-      width: fit-content;
-      background-color: #eaeaeaaf;
-      border-radius: 10px;
-      padding: 0 6px;
-      margin: 8px 0;
-      text-align: center;
-    }
-    .link {
-      text-decoration: none;
-      word-break: break-all;
-      text-decoration: none;
-      color: #676767;
-    }
-  }
-  .sub-text {
-    margin: 2rem 0;
-    text-align: center;
-    word-break: keep-all;
-    color: #888888;
-    font-size: 1rem;
-  }
-
-`
-const Carousel = styled(MuiCarousel)`
-  margin-bottom: 2rem;
-  .slide {
-    display: flex;
-    justify-content: center;
-  }
 `
 
 const ContentBox = styled.div`
@@ -184,19 +97,5 @@ const ContentBox = styled.div`
     background-position: bottom;
     background-size: 100% 50%;
     background-repeat: no-repeat;
-  }
-  @media (max-width: 900px) {
-    width: 70vw;
-    .info-box {
-      flex-direction: column;
-    }
-    h6 {
-      margin: 0.5rem 0;
-      width: 100%;
-    }
-    a, .info-text { 
-      width: 70vw;
-      line-height: 1.3rem;
-    }
   }
 `
